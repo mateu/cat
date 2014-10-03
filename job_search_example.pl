66ee5c847c39bad4507f5c18217b4a825a6ffdb1
Mateu X Hunter <hunter@missoul.org>
2014-10-03 11:05:39 -0600

    Begin to make realistic job search

--- job_search_example.pl
+++ job_search_example.pl
@@ -1,40 +1,71 @@
+#!/usr/bin/env perl
+use 5.014;
+use utf8;
+
 use Search::Elasticsearch;
-my $es = Search::Elasticsearch->new;
+my $es    = Search::Elasticsearch->new;
+my %pesat = (
+  perl        => 10,
+  linux       => 10,
+  catalunya   => 10,
+  javascript  => 5,
+  jQuery      => 7,
+  AJAX        => 5,
+  MySQL       => 5,
+  SQL         => 7,
+  benefits    => 5,
+  wellness    => 10,
+  'laid-back' => 5,
+  amsterdam   => 5,
+);
 
 my $query = {
-  bool => { 
+  bool => {
     should => [
-    {
-      match => {
-        _all => {
-          query => 'pomes',
-          boost => 10,
-        }
-      }
-    },
-    {
-      match => {
-        _all => {
-          query => 'pollastre',
-          boost => 10,
+      {
+        match => {
+          _all => {
+            query => 'javascript',
+            boost => 10,
+          }
         }
       },
-    }
-  ]
+      {
+        match => {
+          _all => {
+            query => 'perl',
+            boost => 10,
+          }
+        },
+      }
+    ]
+  }
+};
+
+$query = {
+  bool => {
+    should => [
+      map { { match => { _all => { query => $_, boost => $pesat{$_}, } } } }
+        keys %pesat
+    ]
   }
 };
 
 my %search_params = (
-    index => 'recepta',
-    type  => 'cat', 
-    body  => {
-        query => {
-            function_score => {
-                query => $query
-            }
-        }
-    },
+  index => 'jobs',
+  type  => 'perl',
+  body  => {
+    query => {
+      function_score => {
+        query => $query
+      }
+    }
+  },
 );
 my $matches = $es->search(%search_params)->{hits}->{hits};
-use Data::Dumper::Concise;
-warn "Matches: ", Dumper $matches;
+
+foreach my $job ( @{$matches} ) {
+  my $fields = $job->{_source};
+  say "Title: ", $fields->{'títol'};
+  say "Score: ", $job->{_score};
+}

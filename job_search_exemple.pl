ebf65ed885a3eb39f11467c40823f128679038c7
Mateu X Hunter <hunter@missoula.org>
2014-10-08 10:39:34 -0600

    afina una mica més

--- job_search_exemple.pl
+++ job_search_exemple.pl
@@ -1,27 +1,28 @@
 #!/usr/bin/env perl
 use 5.014;
 use utf8;
-use Data::Dumper::Concise;
 
 use Search::Elasticsearch;
-my $es = Search::Elasticsearch->new( trace_to => [ 'File', 'es.log' ] );
+my $es = Search::Elasticsearch->new(trace_to => [ 'File', 'es.log' ]);
 my %scale = (
   irrelevant    => 1,
   little        => 1.5,
-  little_neg    => 0.666,
   somewhat      => 3,
+  very          => 20,
+  mandatory     => 100,
+  little_neg    => 0.666,
   somewhat_neg  => 0.5,
-  very          => 10,
   very_neg      => 0.05,
-  mandatory     => 50,
   mandatory_neg => 0,
 );
 my %profile = (
   linux         => 'mandatory',
   telecommute   => 'mandatory',
   elasticsearch => 'mandatory',
-  benefits      => 'very',
+  test          => 'very',
+  cpan          => 'very',
   git           => 'very',
+  debian        => 'very',
   postgresql    => 'very',
   plack         => 'very',
   'node.js'     => 'very',
@@ -29,11 +30,11 @@ my %profile = (
   moo           => 'very',
   moose         => 'very',
   catalyst      => 'very',
-  test          => 'very',
-  cpan          => 'very',
-  debian          => 'very',
-  ubuntu          => 'very',
-  mvc          => 'somewhat',
+  ubuntu        => 'very',
+  remote        => 'very',
+  benefits      => 'very',
+  wellness      => 'very',
+  mvc           => 'somewhat',
   math          => 'somewhat',
   mojolicious   => 'somewhat',
   dancer        => 'somewhat',
@@ -45,21 +46,21 @@ my %profile = (
   sql           => 'somewhat',
   agile         => 'somewhat',
   scrum         => 'somewhat',
-
-  subversion => 'somewhat_neg',
-  apache     => 'somewhat_neg',
-  svn        => 'somewhat_neg',
-  java       => 'somewhat_neg',
-  c          => 'somewhat_neg',
-  cvs        => 'very_neg',
-  microsoft  => 'very_neg',
-  windows    => 'very_neg',
-  oracle     => 'very_neg',
-  eligo      => 'mandatory_neg',
-  taxsys     => 'mandatory_neg',
+  c             => 'little_neg',
+  subversion    => 'little_neg',
+  apache        => 'little_neg',
+  svn           => 'little_neg',
+  java          => 'somewhat_neg',
+  soap          => 'somewhat_neg',
+  cvs           => 'very_neg',
+  microsoft     => 'very_neg',
+  windows       => 'very_neg',
+  oracle        => 'very_neg',
+  eligo         => 'mandatory_neg',
+  taxsys        => 'mandatory_neg',
 );
 
-my $query       = { match_all => {} };
+my $query = { match_all => {} };
 my $functions = [
   map {
     {
@@ -73,7 +74,7 @@ my $functions = [
       },
       boost_factor => $scale{ $profile{$_} },
     },
-  } keys %profile
+    } keys %profile
 ];
 
 my %search_params = (
@@ -91,11 +92,9 @@ my %search_params = (
   },
 );
 my $matches = $es->search(%search_params)->{hits}->{hits};
-if ( ref($matches) and ref($matches) eq 'ARRAY' ) {
-  foreach my $job ( @{$matches} ) {
-    my $fields = $job->{_source};
-    say "Title: ", $fields->{'títol'};
-    say "Score: ", $job->{_score};
-    say "URL: ",   $fields->{url};
-  }
+foreach my $job (@{$matches}) {
+  my $fields = $job->{_source};
+  say "Title: ", $fields->{'títol'};
+  say "Score: ", $job->{_score};
+  say "URL: ",   $fields->{url};
 }
